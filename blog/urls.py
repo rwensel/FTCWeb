8b21553f03577e3342584efe8e1a8d1d27621c53
Richard Wensel <rmwensel@gmail.com>
2021-10-21 19:18:56 -0400

    reorg

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
-from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(
         '',
-        views.BaseAdminDocsView.as_view(template_name='index.html'),
-        name='index-root',
+        TemplateView.as_view(template_name='blog.html'),
+        name='blog-root',
     ),
 ]
--- listener/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import listen
-
-urlpatterns = [
-    path('', listen.listener),
-]
